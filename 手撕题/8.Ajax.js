33bbda7e79d97a1bf76f4eb80a1694aff8862b9a
arc1 <19302806482@163.com>
2025-08-06 00:14:25 +0800

    手写ajax

--- 手撕题/8.Ajax.js
+++ 手撕题/8.Ajax.js
@@ -1,3 +1,22 @@
-function ajax(){
-    
+const xhr=new XMLHttpRequest()
+// 初始化请求，method，url，async
+let method = "GET"
+let url = "https://developer.mozilla.org/"
+xhr.open(method,url,true)
+xhr.onreadystatechange=function(){
+    if(this.readyState!==4) return
+    if(this.status==200){
+        console.log(this.responseText);
+    }else{
+        throw new Error(xhr.statusText)
+    }
 }
+// 发送请求
+xhr.send()
+
+// 静态属性，只能通过XMLHttpRequest.UNSENT这种调用
+// 0：未初始化（UNSENT）
+// 1：已调用 open () 方法（OPENED）
+// 2：已调用 send () 方法（HEADERS_RECEIVED）
+// 3：正在接收响应（LOADING）
+// 4：请求完成（DONE）
